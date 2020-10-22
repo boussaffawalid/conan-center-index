708527ce24780f683746b0f9cd2c9cf3c8160eae
Walid Boussaffa <boussaffa.walid@outlook.com>
2020-10-22 19:54:09 +0100

    added o2 package

--- recipes/o2/all/conanfile.py
+++ recipes/o2/all/conanfile.py
@@ -2,9 +2,6 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-_all_options = ['with_twitter', 'with_dropbox', 'with_google', 'with_vimeo', 'with_facebook', 'with_uber', 'with_skydrive', 
-    'with_flickr', 'with_hubic', 'with_spotify', 'with_surveymonkey', 'with_smugmug', 'with_msgraph', 'with_keychain']
-
 class O2Conan(ConanFile):
     name = "o2"
     license = "BSD 2-Clause \"Simplified\" License"
@@ -13,21 +10,18 @@ class O2Conan(ConanFile):
     topics = ("conan", "network", "OAuth", "qt")
     description = "This library encapsulates the OAuth 1.0 and 2.0 client authentication flows, and the sending of authenticated HTTP requests."
     settings = "os", "compiler", "build_type", "arch"
-
-    options = dict({
+    options = {
         "shared": [True, False],
         "fPIC": [True, False],
         "with_oauth1": [True, False],
-        "with_keychain": [True, False],
-        }, **{option: [True, False] for option in _all_options}
-    )
-    default_options = dict({
+        "with_keychain": [True, False]
+    }
+    default_options = {
         "shared": False,
         "fPIC": True,
-        "with_oauth1": False,
-        "with_keychain": True,
-        }, **{option: False for option in _all_options}
-    )
+        "with_oauth1": True,
+        "with_keychain": True
+    }
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
 
@@ -42,8 +36,9 @@ class O2Conan(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        pass
         #self.requires("qt/5.15.1")
+        if self.options.with_keychain:
+            self.requires("qtkeychain/0.11.1")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -53,11 +48,15 @@ class O2Conan(ConanFile):
         if self._cmake:
             return self._cmake
 
+        _all_options = ['with_twitter', 'with_dropbox', 'with_google', 'with_vimeo', 
+            'with_facebook','with_uber', 'with_skydrive', 'with_flickr', 'with_spotify', 
+            'with_smugmug', 'with_msgraph', 'with_keychain']
+
         self._cmake = CMake(self)
         self._cmake.definitions['o2_WITH_OAUTH1'] = self.options.with_oauth1
         self._cmake.definitions['o2_WITH_KEYCHAIN'] = self.options.with_keychain
         for option in _all_options:
-            self._cmake.definitions['o2_{}'.format(option.upper())] = self.options.get_safe(option)
+            self._cmake.definitions['o2_{}'.format(option.upper())] = True
         self._cmake.configure()
         return self._cmake
 
@@ -69,6 +68,7 @@ class O2Conan(ConanFile):
         cmake = self._configure_cmake()
         cmake.install()
         self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        self.copy("o0export.h", src=os.path.join(self._source_subfolder, "src"), dst="include/o2")
 
     def package_info(self):
         self.cpp_info.libs = ["o2"]
